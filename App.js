6c79936e5d6a450af2b5e441fcc8d68d19593688
Brandon Y Lam <me@brandonlam.ca>
2020-06-10 14:06:02 -0700

    fetch pokemon and save in state

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './Header';
 
+const POKEMON_API_ENDPOINT = `https://pokeapi.co/api/v2/`;
+const FIRST_100_QUERY = `pokemon?limit=100`;
+
 export default function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${POKEMON_API_ENDPOINT}${FIRST_100_QUERY}`)
+      .then(res => res.json())
+      .then(payload => {
+        console.log(payload)
+        setPokemonList(payload.results)
+      })
+  }, [])
+
   return (
     <View style={styles.container}>
       <Header/>
