7c6b94d52199aa5060655e359e6a4cb12895a0f9
Brandon Y Lam <me@brandonlam.ca>
2020-06-09 21:28:33 -0700

    testing stuff

--- App.js
+++ App.js
@@ -1,10 +1,36 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, ScrollView, TouchableHighlight } from 'react-native';
+
+const POKEMON_API_ENDPOINT = "https://pokeapi.co/api/v2/"
+const GET_50_POKEMON_PATH = "pokemon?limit=50"
 
 export default function App() {
+  const [pokemonList, setPokemonList] = useState([]);
+  useEffect(() => {
+    fetch(`${POKEMON_API_ENDPOINT}${GET_50_POKEMON_PATH}`)
+      .then(res => res.json())
+      .then(payload => {
+        setPokemonList(payload.results)
+      })
+  }, [])
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.header}>
+        <Text style={styles.h1}>Pokédex</Text>
+      </View>
+      <View style={styles.pokemonList}>
+        <ScrollView>
+          {pokemonList.map((pokemon, i) => {
+            return(
+              <TouchableHighlight key={i} onPress={() => { console.log('tapped') }} underlayColor="lightblue">
+                <View style={styles.item}>
+                  <Text style={styles.h2}>{pokemon.name}</Text>
+                </View>
+              </TouchableHighlight>
+            )
+          })}
+        </ScrollView>
+      </View>
     </View>
   );
 }
@@ -13,7 +39,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  header: {
+    paddingTop: 30,
+    flex: 2,
+    backgroundColor: '#ee1515',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center',
+    borderBottomColor: 'black',
+    borderBottomWidth: 5
+  },
+  h1: {
+    fontSize: 50
+  },
+  pokemonList: {
+    width: '100%',
+    flex: 8
+  },
+  item: {
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    height: 50,
+    borderBottomColor: 'lightgrey',
+    borderBottomWidth: 1
+  }
 });
